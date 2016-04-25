f20f93e1de5bc460e1b153060a39ea6ca9af2bbf
abraxasrex <jbreasor@gmail.com>
2016-04-24 18:49:03 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# River of Text
+
+A plugin for creating scrolling text inside of div or frame on your page.  
+
+How to use:  
+
+1. set an id of 'river-container' on the element you want to display text.  
+- you may have to set a z-index on the container to make sure you don't overlap other elements  
+- inspect the code if you want to know/change the styling that gets set on the container.
+-know that this works by appending p tags to your page  
+ 
+2.  src the script (WIP)  
+
+3. call the function createRiver() like so:  
+``` js
+createRiver(minFontSize, maxFontSize, userColor, containerWidth, containerHeight, codeStrings)
+```  
+- the arguments represent minimum font, max font, text color, width of the animate container, the height of the animate container, and an array of values to display as text.  
+- If you don't provide args, or the right kind of arg, it will default to some other values. If you include arugments, enter something for all 6 of them!  
---
+++ river.js
@@ -0,0 +1,105 @@
+// var codeStrings = [
+//   '(function(){a=b})(),a=a)',
+//   'a=a=b',
+//   'for(vari=0;i<arr.length;i++)',
+//   'if(cat="orange"){lasagna.eat}',
+//   'typeof NaN==="number"',
+//   '5!=="5"',
+//   '7=="7"',
+//   '[{prp:0},{prp:1}]',
+//   '!(x!==y)||false',
+//   'o[p].q>=r+s',
+//   '01001',
+//   '0110010110'
+// ];
+
+var createRiver = function(minFontSize, maxFontSize, userColor, containerWidth, containerHeight, codeStrings){
+
+if(typeof minFontSize !== 'number' || typeof maxFontSize !== 'number'){
+minFontSize = 10;
+maxFontSize = 30;
+console.log('sorry, your font input was not a number. I set it for you.');
+}
+
+if(typeof containerWidth || typeof containerHeight !== 'number'){
+ containerWidth = 800;
+ containerHeight = 400;
+ console.log('sorry, your container input was not a number. I set it for you.')
+}
+
+if(typeof userColor !== 'string'){
+  userColor = 'black';
+  console.log('sorry, color should be a string! I set it for you.')
+}
+
+if(!Array.isArray(codeStrings)){
+  codeStrings = [
+    '(function(){a=b})(),a=a)',
+    'a=a=b',
+    'for(vari=0;i<arr.length;i++)',
+    'if(cat="orange"){lasagna.eat}',
+    'typeof NaN==="number"',
+    '5!=="5"',
+    '7=="7"',
+    '[{prp:0},{prp:1}]',
+    '!(x!==y)||false',
+    'o[p].q>=r+s',
+    '01001',
+    '0110010110'
+  ];
+  console.log('sorry, ur input was not an array. I set it for you.');
+}
+
+var riverContainer = document.getElementById('river-container');
+var parentWidth = riverContainer.style.width;
+function formatContainer(){
+  riverContainer.style.position = 'relative';
+  riverContainer.style.top = '0';
+  riverContainer.style.left = '0';
+  riverContainer.style.width = containerWidth + 'px';
+  riverContainer.style.height = containerHeight + 'px';
+  riverContainer.padding = '0px';
+  riverContainer.margin = '0px';
+  riverContainer.parentNode.style.overflow = 'hidden';
+  riverContainer.style.overflow = 'hidden';
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function jsAnimate(){
+  var children = riverContainer.children;
+  if(children.length){
+    for(var i = 0; i < children.length; i ++){
+      children[i].style.left = parseInt(children[i].style.left.slice(0, -2)) + 1 + 'px';
+
+      if(parseInt(children[i].style.left.slice(0, -2)) > 1500){
+         riverContainer.removeChild(children[i]);
+     }
+    }
+  }
+}
+
+function loopAnim (){
+  var p = document.createElement('p');
+  var randomSize = getRandom(minFontSize, maxFontSize);
+  var randomY = getRandom(0, 800);
+  var randomI = getRandom(0, codeStrings.length - 1);
+  p.textContent = codeStrings[randomI];
+  p.style.fontSize = randomSize + 'px';
+  p.style.top = randomY + 'px';
+  p.style.color = userColor;
+  p.style.left = '10px';
+  p.style.background = 'rgba(0,0,0,0)';
+  p.style.position = 'absolute';
+  p.style.margin = '0';
+  p.style.padding = '0';
+  p.classList.add('code-piece');
+  riverContainer.appendChild(p);
+}
+
+formatContainer();
+setInterval(loopAnim, 500);
+setInterval(jsAnimate, 25);
+}
